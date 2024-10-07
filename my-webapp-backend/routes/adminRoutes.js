5f09bc88b2f9e6942ead2c804ba736b749151ace
Fathanmubina <mubinaf29@gmail.com>
2024-10-07 11:02:02 +0700

    +

--- my-webapp-backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../config/middleware');
-
-router.get('/admin', authMiddleware(['admin']), (req, res) => {
-    res.json({ message: 'Hello, Admin' });
-});
-
-module.exports = router;
--- my-webapp-backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/member', authMiddleware(['member', 'admin']), (req, res) => {
-    res.json({ message: 'Hello, Member' });
-});
-
-module.exports = router;
